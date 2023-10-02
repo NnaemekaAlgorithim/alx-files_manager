5b3ed47d9673ff026cbe88c5dd3e659ccc994b18
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2023-10-02 13:25:12 +0100

    Completing the JS project

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import redisClient from './utils/redis';
+import redis from 'redis';
+const redisClient = redis.createClient();
 
 (async () => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
+  console.log(redisClient.isAlive());
+  console.log(await redisClient.get('myKey'));
+  await redisClient.set('myKey', 12, 5);
+  console.log(await redisClient.get('myKey'));
 
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
+  setTimeout(async () => {
+    console.log(await redisClient.get('myKey'));
+  }, 1000 * 10);
 })();
